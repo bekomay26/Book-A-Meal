634c978b1ce741e21d1574a2b9380525d0082729
Folajimi Ogunbadejo <bekomay26@gmail.com>
2018-04-25 23:24:38 +0100

    [#156803942] add Menu model and fake data files

---
+++ server/models/Menu.js
@@ -0,0 +1,10 @@
+class Menu {
+  constructor(id, date, meals, createdBy, editedBy) {
+    this.id = id;
+    this.date = date;
+    this.meals = meals;
+    this.createdBy = createdBy;
+    this.editedBy = editedBy;
+  }
+}
+export default Menu;
