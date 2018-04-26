029d73c3dd3c50723f4720e6dd22127f0ebb4218
Folajimi Ogunbadejo <bekomay26@gmail.com>
2018-04-26 13:47:24 +0100

    [#157107987] add an object with the

--- server/tests/dummyData/fakeMenus.js
+++ server/tests/dummyData/fakeMenus.js
@@ -2,7 +2,33 @@ import faker from 'faker';
 
 const menus = [{
   id: 2111,
-  date: '26/14/2221',
+  date: '26/14/2221', // use yyyy-mm-dd format
+  meals: [ // would be mealsId in database
+    {
+      id: 1222,
+      name: 'Black Soup',
+      description: faker.lorem.sentence(),
+      category: 'Snacks', // try enums for db
+      price: 870,
+      image_url: faker.image.imageUrl(),
+      extras: ['Garri', 'Amala', 'Akpu'], // extras should have its own table in db
+    },
+    {
+      id: 1223,
+      name: 'White Soup',
+      description: faker.lorem.sentence(),
+      category: 'Snacks', // try enums for db
+      price: 970,
+      image_url: faker.image.imageUrl(),
+      extras: ['Garri', 'Amala', 'Akpu'], // extras should have its own table in db
+    },
+  ],
+  createdBy: faker.name.firstName(),
+  editedBy: [faker.name.lastName(), faker.name.lastName()], // More than one person can edit a menu
+},
+{
+  id: 2112,
+  date: '2019/02/14', // use these format
   meals: [ // would be mealsId in database
     {
       id: 1222,
