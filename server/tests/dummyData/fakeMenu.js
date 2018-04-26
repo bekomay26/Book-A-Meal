b917811accdf5ce09975f7c4f22e07d92b86d125
Folajimi Ogunbadejo <bekomay26@gmail.com>
2018-04-26 01:15:02 +0100

    [#156803942] add dummy data for a day's menu

---
+++ server/tests/dummyData/fakeMenu.js
@@ -0,0 +1,24 @@
+import faker from 'faker';
+// A single menu -- Array of meals
+const menu = [
+  {
+    id: 1222,
+    name: 'Black Soup',
+    description: faker.lorem.sentence(),
+    category: 'Snacks',
+    price: 870,
+    image_url: faker.image.imageUrl(),
+    extras: ['Garri', 'Amala', 'Akpu'],
+  },
+  {
+    id: 1223,
+    name: 'White Soup',
+    description: faker.lorem.sentence(),
+    category: 'Snacks', // try enums for db
+    price: 970,
+    image_url: faker.image.imageUrl(),
+    extras: ['Garri', 'Amala', 'Akpu'],
+  },
+];
+
+export default menu;
