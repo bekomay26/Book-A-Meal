2a9fe3eddbe028fc0b075683d2845d4025434cb6
Folajimi Ogunbadejo <bekomay26@gmail.com>
2018-04-26 22:34:07 +0100

    [#156804056] add tests

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -5,7 +5,8 @@ const orderRouter = express.Router();
 
 orderRouter.route('/')
   .post(OrderController.createOrder);
-
+orderRouter.route('/:id')
+  .put(OrderController.updateOrder);
 
 // mealRouter.post('/', () => {
 //   MealController.createMeal;
--- server/tests/Routes/order.routes.test.js
+++ server/tests/Routes/order.routes.test.js
@@ -39,3 +39,64 @@ describe('/POST order', () => {
       });
   });
 });
+
+describe('/PUT order', () => {
+  it('it should return an error for invalid input', (done) => {
+    const meal = {
+      extras: ['rice', 'potato'],
+      qty: 's1',
+    };
+    request(app)
+      .put('/api/v1/orders/3111')
+      .send(meal)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.message).to.equal('Input a valid quantity');
+        done();
+      });
+  });
+  it('it should edit the order', (done) => {
+    const meal = {
+      extras: ['rice', 'potato'],
+      qty: 2,
+    };
+    request(app)
+      .put('/api/v1/orders/3111')
+      .send(meal)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('Order Updated');
+        expect(res.body.Order.meal.qty).to.equal(meal.qty);
+        done();
+      });
+  });
+  // To cover the branch on line 56 of controller
+  it('it should edit the order if extras is not given', (done) => {
+    const meal = {
+      qty: 2,
+    };
+    request(app)
+      .put('/api/v1/orders/3111')
+      .send(meal)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.message).to.equal('Order Updated');
+        expect(res.body.Order.meal.qty).to.equal(meal.qty);
+        done();
+      });
+  });
+  it('it should give a not found error', (done) => {
+    const meal = {
+      extras: ['rice', 'potato'],
+      qty: 2,
+    };
+    request(app)
+      .put('/api/v1/orders/3114')
+      .send(meal)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(404);
+        expect(res.body.message).to.equal('Cannot find order with id 3114');
+        done();
+      });
+  });
+});
