347eb7edfad07331d7ebe9ad2f8bda89990d6439
Folajimi Ogunbadejo <bekomay26@gmail.com>
2018-05-04 08:54:05 +0100

    [#157243679] make provisions for unserved endpoints

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mealRouter from './routes/mealRoutes';
 import menusRouter from './routes/menusRoutes';
 import orderRouter from './routes/orderRoutes';
+import errorRouter from './routes/errorRoutes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,11 +14,16 @@ app.use(bodyParser.json());
 app.use('/api/v1/meals', mealRouter);
 app.use('/api/v1/menu', menusRouter);
 app.use('/api/v1/orders', orderRouter);
-
-
 app.get('/', (req, res) => {
   res.send('Hello, world');
 });
+
+app.use('/*', errorRouter);
+
+// app.get('/*', (req, res) => {
+//   res.status(404).send('Page not found');
+// });
+
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
